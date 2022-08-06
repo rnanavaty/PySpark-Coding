6e102bbdb35412c27f616da1afde6e1a30e45633
Rahul <booksmagica@gmail.com>
2022-08-06 15:35:03 +0530

    Create RDD_Code.py

---
+++ RDD_Code.py
@@ -0,0 +1,43 @@
+#Example-1
+# Create RDD - parallelize(<arguments>) a collection (list or an array of some elements)
+print("Output of example 1 is : ")
+data = sc.parallelize(
+    [('Amy', 22), ('Bond', 23), ('Cathy',30), ('Donna', 40)])
+print(data)
+print(data.collect())
+
+#Example-2
+#Various type of data in parallelize
+#Multiple datatypes can be mixed: a tuple, a dict, or a list
+print("\nOutput of example 2 is : ")
+data_heterogenous = sc.parallelize([
+ ('Car', 'Jet'),
+ {'Speed': 100000},
+ ['Australia','visited', 2022]
+]).collect()
+print(data_heterogenous)
+
+##Example-3
+#Left Join transformation and intersection
+print("\nOutput of example 3 is : ")
+rdd1 = sc.parallelize([('a', 1), ('b', 4), ('c',10)])
+rdd2 = sc.parallelize([('a', 4), ('a', 1), ('b', '6'), ('d', 15)])
+rdd3 = rdd1.leftOuterJoin(rdd2)
+print(rdd3.collect())
+rdd4 = rdd1.intersection(rdd2)
+print(rdd4.collect())
+
+###############################################################################
+# Output                                                                      #
+###############################################################################
+
+Output of example 1 is : 
+ParallelCollectionRDD[51] at readRDDFromFile at PythonRDD.scala:274
+[('Amy', 22), ('Bond', 23), ('Cathy', 30), ('Donna', 40)]
+
+Output of example 2 is : 
+[('Car', 'Jet'), {'Speed': 100000}, ['Australia', 'visited', 2022]]
+
+Output of example 3 is : 
+[('b', (4, '6')), ('c', (10, None)), ('a', (1, 4)), ('a', (1, 1))]
+[('a', 1)]
